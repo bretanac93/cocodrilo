5e00f95bc7000615df777fff04c56cb200a0b85d
Israel Michel Bernal <imbernal9203@gmail.com>
2015-09-07 15:34:02 +0200

    FOSUserBundle working

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Cocodrilo\AppBundle\AppBundle(),
+            new Cocodrilo\UserBundle\UserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
